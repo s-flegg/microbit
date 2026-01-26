fe2f44e8753e364798055435e70d533dbb278015
s-flegg <161213652+s-flegg@users.noreply.github.com>
2026-01-26 21:03:03 +0000

    Threading reversion, minor fixes, button termination, reduced error chance
    Threading has been removed from the server as it cannot be used on a microbit. The server itself has had button functionality added so pressing A will end uart transmissions. For the PC side of uart, threading has been added to allow the RECEIVED message to be sent continually, until a new message is received. This will ensure the RECEIVE message itself of received by the server.

--- Server_main.py
+++ Server_main.py
@@ -1,5 +1,4 @@
 from microbit import *
-import threading
 from uart_microbit import Communicate
 import radio
 
@@ -7,9 +6,8 @@ radio.on()
 radio.config(length=251)
 
 header = "ID, m_type, time, level, sessionID, accel, warning\n"
-# used to share data between threads
-data = []
 
+uart = Communicate(False, True)
 
 def check_message_format(message):
     if "," in message:
@@ -17,30 +15,14 @@ def check_message_format(message):
     else:
         return False
 
-def radio_output():
-    while True:
+while True:
 
-        message = radio.receive()
-        data.append(message)
+    message = radio.receive()
 
-        if message and check_message_format():
-            # Debug to show when message is received
-            display.show("R")
+    if message and check_message_format(message):
+        # Debug to show when message is received
+        display.show("R")
 
-            send_message = header + message
+        send_message = header + message
 
-            data.append(send_message)
-
-def comms():
-    uart = Communicate(False)
-
-    while True:
-        while len(data) > 0: # looping here will ensure termination doesn't happen while there are messages waiting to be sent
-            uart.send(data.pop(0))
-        uart.button_terminate()
-
-t1 = threading.Thread(target=radio_output)
-t2 = threading.Thread(target=comms)
-
-t1.start()
-t2.start()
+        uart.send(send_message)
