1f073aa4e1c85da54f972a6a93135dfa242ec935
s-flegg <161213652+s-flegg@users.noreply.github.com>
2026-01-26 13:42:04 +0000

    Threading
    Added threading to run the uart and radio simultaneously

--- Server_main.py
+++ Server_main.py
@@ -1,31 +1,46 @@
 from microbit import *
+import threading
+from uart_microbit import Communicate
 import radio
-
 
 radio.on()
 radio.config(length=251)
+
+header = "ID, m_type, time, level, sessionID, accel, warning\n"
+# used to share data between threads
+data = []
+
 
 def check_message_format(message):
     if "," in message:
         return True
     else:
         return False
-        
-  
-header =  "ID, m_type, time, level, sessionID, accel, warning\n"
-   
-while True:
-    
-    message = radio.receive()
 
-    
-    if message and check_message_format():
-        #Debug to show when message is received
-        display.show("R")
-        
-        send_message = header + message
-        
-        radio.send(send_message)
-        
-        
-    sleep(500)
+def radio_output():
+    while True:
+
+        message = radio.receive()
+        data.append(message)
+
+        if message and check_message_format():
+            # Debug to show when message is received
+            display.show("R")
+
+            send_message = header + message
+
+            data.append(send_message)
+
+def comms():
+    uart = Communicate(False)
+
+    while True:
+        while len(data) > 0: # looping here will ensure termination doesn't happen while there are messages waiting to be sent
+            uart.send(data.pop(0))
+        uart.button_terminate()
+
+t1 = threading.Thread(target=radio_output)
+t2 = threading.Thread(target=comms)
+
+t1.start()
+t2.start()
